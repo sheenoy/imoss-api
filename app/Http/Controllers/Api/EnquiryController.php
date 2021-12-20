fd555551f2d317897b95b3e882dcbc414a5e4c4c
Shoaib <shoaib2fly@gmail.com>
2021-12-20 19:08:51 +0530

    Applied filters in Ad list API

--- app/Http/Controllers/Api/EnquiryController.php
+++ app/Http/Controllers/Api/EnquiryController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreEnquiryRequest;
 use App\Models\{Enquiry};
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EnquiryResponseToUser;
 
 class EnquiryController extends Controller
 {
@@ -19,6 +21,13 @@ class EnquiryController extends Controller
                 ];
         $save_enquiry = Enquiry::insert($data);
         if ($save_enquiry) {
+            try{
+                $thank_you_email = Mail::to($request->get('email'))
+                                    ->bcc("shoaib2fly@gmail.com")
+                                    ->send(new EnquiryResponseToUser());
+            } catch(\Exception $e) {
+
+            }
             return response()->json(['message' => "Form submitted successfully!!"]);
         } else {
             return response()->json(['message' => "Oops! Server unreachable at the moment, please try again later"], 400);
---
+++ resources/views/enquiry-response.blade.php
@@ -0,0 +1,8 @@
+Bonjour,
+
+Nous avons bien reçu votre correspondance et reviendrons vers vous dans les plus brefs délais. Merci de nous avoir contacté.
+
+Bien cordialement,
+
+L'agence 
+IMOSS IMMOBILIER
